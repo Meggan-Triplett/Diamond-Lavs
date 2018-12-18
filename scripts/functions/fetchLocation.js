5447987ecf393c6f822c2f83746b5debfaec218e
Gwendolyn Zubatch <gyzubatch@gmail.com>
2018-12-18 13:10:10 -0800

    map now renders with address search

--- scripts/functions/fetchLocation.js
+++ scripts/functions/fetchLocation.js
@@ -15,8 +15,13 @@
 
 
 // FUNCTION:
+const superagent = require('superagent');
+const getLavs = require('./scripts/functions/getLavs');
+
 function fetchLocation (request,response) {  // change (request) to (request,response) before running
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`; // hard-code API key to test
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.address}&key=${process.env.GEOCODE_API_KEY}`; // hard-code API key to test
+  console.log('query = ',request.query.address);
+  console.log('url = ',url);
   return superagent.get(url)
     .then( apiData => {
       // if no data: throw error
@@ -26,14 +31,15 @@ function fetchLocation (request,response) {  // change (request) to (request,res
       } else {
         let location = {lat: apiData.body.results[0].geometry.location.lat, lng: apiData.body.results[0].geometry.location.lng};
         console.log(`location: lat = ${location.lat}; location:lng = ${location.lng}`);
-        // let lavs = getLavs(location);
-        response.render(('/pages/index'), {lat: location.lat, lng: location.lng, pagename: 'urhere'});  // un-comment before running
+        let lavs = getLavs(location);
+        response.render(('./pages/index'), {lat: location.lat, lng: location.lng, pagename: 'urhere'});  // un-comment before running
       }
     })
     .catch( error => handleError(error)); // un-comment before running
 }
 
-module.exports.fetchLocation = fetchLocation;
+
+module.exports = fetchLocation;
 
 
 
