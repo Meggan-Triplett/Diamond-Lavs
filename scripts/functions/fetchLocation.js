2b9610106a3d85bc6755e499050b242e252ccb29
rjjohnson1204 <rjjohnson1204@gmail.com>
2018-12-18 09:42:53 -0800

    experimenting with google maps

--- scripts/functions/fetchLocation.js
+++ scripts/functions/fetchLocation.js
@@ -1,43 +1,97 @@
 'use strict';
 
-// DESCRIPTION:
-// This function accepts a user-defined location search, requests location from Google Geocoding, and returns lat and lng to client.
+// // DESCRIPTION:
+// // This function accepts a user-defined location search, requests location from Google Geocoding, and returns lat and lng to client.
 
-// INPUTS:
-// (request, response) request.body.search_query  -  format: '2901 3rd ave'
+// // INPUTS:
+// // (request, response) request.body.search_query  -  format: '2901 3rd ave'
 
-// OUTPUTS:
-// response.render( (route), {lat: 47.618294, lng: -122.351190, lavs: lavs )
-//      where "lavs" is an array of 5 lav objects: [{lav_1},{lav_2},{lav_3},{lav_4},{lav_5}]
+// // OUTPUTS:
+// // response.render( (route), {lat: 47.618294, lng: -122.351190, lavs: lavs )
+// //      where "lavs" is an array of 5 lav objects: [{lav_1},{lav_2},{lav_3},{lav_4},{lav_5}]
 
-// CHANGE LOG:
-// 12-17-2018 12:30pm (Gwen) Initial build and test.
+// // CHANGE LOG:
+// // 12-17-2018 12:30pm (Gwen) Initial build and test.
 
 
-// FUNCTION:
-function fetchLocation (request,response) {  // change (request) to (request,response) before running
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`; // hard-code API key to test
-  return superagent.get(url)
-    .then( apiData => {
-      // if no data: throw error
-      if (!apiData.body.results.length) {
-        throw 'No Data from API'
-        // if data: save, send to front
-      } else {
-        let location = {lat: apiData.body.results[0].geometry.location.lat, lng: apiData.body.results[0].geometry.location.lng};
-        console.log(`location: lat = ${location.lat}; location:lng = ${location.lng}`);
-        let lavs = getLavs(location);
-        response.render(('/urhere'), {lat: location.lat, lng: location.lng, lavs: lavs});  // un-comment before running
-      }
+// // FUNCTION:
+// function fetchLocation (request,response) {  // change (request) to (request,response) before running
+//   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`; // hard-code API key to test
+//   return superagent.get(url)
+//     .then( apiData => {
+//       // if no data: throw error
+//       if (!apiData.body.results.length) {
+//         throw 'No Data from API'
+//         // if data: save, send to front
+//       } else {
+//         let location = {lat: apiData.body.results[0].geometry.location.lat, lng: apiData.body.results[0].geometry.location.lng};
+//         console.log(`location: lat = ${location.lat}; location:lng = ${location.lng}`);
+//         let lavs = getLavs(location);
+//         response.render(('/urhere'), {lat: location.lat, lng: location.lng, lavs: lavs});  // un-comment before running
+//       }
+//     })
+//     .catch( error => handleError(error)); // un-comment before running
+// }
+
+
+
+// // TEST:
+// // let request = {query: { data: '2901 3rd ave'} };
+
+// // fetchLocation (request);
+
+
+
+function fetchLocation(event) {
+  event.preventDefault();
+  let searchQuery = $('#input-search').val();
+
+  $.ajax({
+    url: `${__API_URL__}/location`,
+    method: 'GET',
+    data: {data: searchQuery}
+  })
+    .then(location => {
+      displayMap(location);
+      getResource('weather', location);
+      getResource('movies', location);
+      getResource('yelp', location);
+      getResource('meetups', location);
+      getResource('trails', location);
     })
-    .catch( error => handleError(error)); // un-comment before running
+    .catch(error => {
+      compileTemplate([error], 'error-container', 'error-template');
+      $('#map').addClass('hide');
+      $('section, div').addClass('hide');
+    });
 }
 
+function displayMap(location) {
+  $('.query-placeholder').text(`Here are the results for ${location.formatted_query}`);
 
+  $('#map').removeClass('hide');
+  $('section, div').removeClass('hide');
 
-// TEST:
-// let request = {query: { data: '2901 3rd ave'} };
+  $('#map').attr('src', `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude}%2c%20${location.longitude}&zoom=13&size=600x300&maptype=roadmap
+  &key=AIzaSyDp0Caae9rkHUHwERAFzs6WN4_MuphTimk`)
+}
 
-// fetchLocation (request);
+function getResource(resource, location) {
+  $.get(`${__API_URL__}/${resource }`, {data: location})
+    .then(result => {
+      compileTemplate(result, `${resource}-results`, `${resource}-results-template`);
+    })
+    .catch(error => {
+      compileTemplate([error], 'error-container', 'error-template');
+    })
+}
 
+function compileTemplate(input, sectionClass, templateId) {
+  $(`.${sectionClass}`).empty();
 
+  let template = Handlebars.compile($(`#${templateId}`).text());
+
+  input.forEach(element => {
+    $(`.${sectionClass}`).append(template(element));
+  })
+}
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 'use strict';
-
 // get dependencies
 const express = require ('express');
 const ejs = require ('ejs');
@@ -11,6 +10,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+
 
 // set environment vars
 require('dotenv').config();
@@ -41,6 +41,61 @@ client.connect();
 client.on('error', err => console.error(err));
 
 
+
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
+
+function getLocation(request, response) {
+  Location.lookupLocation({
+    tableName: Location.tableName,
+
+    query: request.query.data,
+
+    cacheHit: function(result) {
+      response.send(result.rows[0]);
+    },
+
+    cacheMiss: function() {
+      const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${this.query}&key=${process.env.GEOCODE_API_KEY}`;
+
+      return superagent.get(url)
+        .then(result => {
+          const location = new Location(this.query, result);
+          // location.save()
+            // .then(location => response.send(location));
+        })
+        .catch(error => handleError(error));
+    }
+  })
+}
+
+function handleError(err, res) {
+  console.error(err);
+  if (res) res.status(500).send('Sorry, something went wrong');
+}
+
+function Location(query, res) {
+  this.search_query = query;
+  // this.formatted_query = res.body.results[0].formatted_address;
+  // this.latitude = res.body.results[0].geometry.location.lat;
+  // this.longitude = res.body.results[0].geometry.location.lng;
+  this.created_at = Date.now();
+}
+
+Location.lookupLocation = (location) => {
+  const SQL = ``;
+  const values = [location.query];
+
+  return client.query(SQL, values)
+    .then(result => {
+      if(result.rowCount > 0) {
+        location.cacheHit(result);
+      } else {
+        location.cacheMiss();
+      }
+    })
+    .catch(console.error);
+}
+
+app.get('/', getLocation);
