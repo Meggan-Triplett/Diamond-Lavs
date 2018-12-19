f2497070a230848e77d99286406480dde5e400bd
Megga-Miister <mtriplett@ISOLALT-64.localdomain>
2018-12-19 00:27:58 -0800

    moved Google Places API query parameters from local scope fetchAPI to global

--- scripts/functions/fetchAPI.js
+++ scripts/functions/fetchAPI.js
@@ -5,29 +5,21 @@
 
 // INPUT:
 // (request, response)
-//  request composition: location ${ lat,lng }, radius ${ desired meters < 50000 }, keyword ${ desired company }, key ${ api key }
+//  input composition: location ${ lat,lng }, radius ${ desired meters < 50000 }, keyword ${ desired company }, key ${ api key }
 
 // OUTPUT:
-// response object from api
+// raw data object from Google Places API
 
 // CHANGE LOGS:
 // 12-18-2018 2:00pm (Meggan) Initial build and test.
 // 12-18-2018 3:40pm (Meggan) Troubleshoot function issue.
-// 12-18-2018 5:000pm (Meggan) Troubleshoot function issue.
+// 12-18-2018 5:000pm (Meggan) Completed function
 
 // FUNCTION:
 const superagent = require('superagent');
 require('dotenv').config();
 
 function fetchAPI () {
-  let search_query = {
-    location: [{
-      lat: 47.6100898,
-      lng: -122.3424699,
-      radius: 5000,
-      keyword: ['Starbucks'],
-    }]
-  };
   const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${search_query.location.lat},${search_query.location.lng}&radius=${search_query.location.radius}&keyword=${search_query.location.keyword}&key=${process.env.GEOCODE_API_KEY}`;
   return superagent.get(url)
   .then ( apiData => apiData)
@@ -35,6 +27,6 @@ function fetchAPI () {
 }
 
 // TEST:
-// calling fetchAPI should return a value
+// calling fetchAPI should return the raw data from the Google Places API
 
 // fetchAPI ();
