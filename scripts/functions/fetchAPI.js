d3dd990b5b136b300d5e116f7f7ed75371deb8a0
Megga-Miister <mtriplett@ISOLALT-64.localdomain>
2018-12-18 17:16:56 -0800

    completed function to pull raw data from Places API

--- scripts/functions/fetchAPI.js
+++ scripts/functions/fetchAPI.js
@@ -11,25 +11,30 @@
 // response object from api
 
 // CHANGE LOGS:
+// 12-18-2018 2:00pm (Meggan) Initial build and test.
+// 12-18-2018 3:40pm (Meggan) Troubleshoot function issue.
+// 12-18-2018 5:000pm (Meggan) Troubleshoot function issue.
 
 // FUNCTION:
-function fetchAPI (request, response) {
-  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${request.search_query.lat},${request.search_query.lng}&radius=${request.search_query.radius}&keyword=${request.search_query.keyword}&key=${process.env.GEOCODE_API_KEY}`;
+const superagent = require('superagent');
+require('dotenv').config();
+
+function fetchAPI () {
+  let search_query = {
+    location: [{
+      lat: 47.6100898,
+      lng: -122.3424699,
+      radius: 5000,
+      keyword: ['Starbucks'],
+    }]
+  };
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${search_query.location.lat},${search_query.location.lng}&radius=${search_query.location.radius}&keyword=${search_query.location.keyword}&key=${process.env.GEOCODE_API_KEY}`;
   return superagent.get(url)
-    .then ( apiLocationsData => {
-      // throw error if the API returns no data
-      if (!apiLocationsData.body.results.length) {
-        throw 'No Data from Places API'
-      } else {
-        let apiResults = apiLocationsData.body;
-        console.log(apiResults);
-        return apiResults;
-      }
-    })
-    // .catch( error => handleError(error));
+  .then ( apiData => apiData)
+  .catch( error => handleError(error));
 }
 
 // TEST:
-// let request = {search_query: { lat: '47.6100898' } { lng: '-122.3424699 } { radius: '5000' } { keyword: 'Starbucks' }}
+// calling fetchAPI should return a value
 
-// fetchAPI (request);
+// fetchAPI ();
